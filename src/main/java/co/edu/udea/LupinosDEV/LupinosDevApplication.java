db8a45c72f78fbd856a2b7d4e09b474c7269ea58
Javier Villabon <javiervillabon2003@gmail.com>
2022-08-19 20:02:56 -0500

    principal
    proyecto principal

---
+++ src/main/java/co/edu/udea/LupinosDEV/LupinosDevApplication.java
@@ -0,0 +1,15 @@
+package co.edu.udea.LupinosDEV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
+public class LupinosDevApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LupinosDevApplication.class, args);
+	}
+
+}
