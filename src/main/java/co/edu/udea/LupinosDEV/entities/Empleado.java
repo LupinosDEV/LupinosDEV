c80f199cd07ce665c93f44b7416a488c40654586
dayanna <dayanz.vg@gmail.com>
2022-09-07 10:38:03 -0500

    Clase_Empleado

--- src/main/java/co/edu/udea/LupinosDEV/entities/Empleado.java
+++ src/main/java/co/edu/udea/LupinosDEV/entities/Empleado.java
@@ -1,18 +1,24 @@
 package co.edu.udea.LupinosDEV.entities;
 
+import javax.persistence.*;
+@Entity
+@Table(name = "Empleado")
 public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
     private String nombreEmpleado;
     private String correoEmpleado;
     private Empresa empresa;
     private String rolEmpleado;
-
+    //Constructor
     public Empleado(String nombreEmpleado, String correoEmpleado, Empresa empresa, String rolEmpleado){
 
         this.nombreEmpleado = nombreEmpleado;
         this.correoEmpleado = correoEmpleado;
         this.empresa = empresa;
         this.rolEmpleado = rolEmpleado;
-
     }
 
     public String getNombreEmpleado() {
