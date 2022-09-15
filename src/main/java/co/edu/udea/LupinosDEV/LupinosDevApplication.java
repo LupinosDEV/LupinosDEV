ebb48e21145e11db46f708a7a31bbebab640fd3a
Javier Villabon <javiervillabon2003@gmail.com>
2022-09-15 13:28:55 -0500

    los datos persisten en una base de datos y se puede crear transactions

--- src/main/java/co/edu/udea/LupinosDEV/LupinosDevApplication.java
+++ src/main/java/co/edu/udea/LupinosDEV/LupinosDevApplication.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class LupinosDevApplication {
+	/*
+	PARA QUE FUNCIONE DEBE CREAR UN EMPLEADO Y EN LA TABLA MOVIMIENTO DINERO EN EL CAMPO empleado_id
+	darle el id del empleado creado
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(LupinosDevApplication.class, args);
 	}
